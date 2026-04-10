7e4aed85ba9d6fc05f8f1e8c21f91eff5cc3f5c3
Vadym4ik33 <novoselcevsovin@gmail.com>
2026-04-10 16:27:17 +0300

    First commit

---
+++ resourcemon.py
@@ -0,0 +1,19 @@
+import psutil
+import time
+
+# [|||||.....]
+
+while True:
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory()
+
+    time.sleep(5)
+
+    count = int(ram.percent / 10) 
+
+    # Рисуем: (палочка * кол-во) + (точка * (10 - кол-во))
+    bar = f"[{'|' * count}{'.' * (10 - count)}]"
+
+    print(f"{ram.percent}% {bar}")
+
+    print(cpu)
